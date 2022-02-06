1b8a7a5887208d401aac174d324f7847960f3ba1
Yetife <tifetreats21@gmail.com>
2022-02-06 02:04:31 +0100

    Added new kotlin exercise

---
+++ src/main/kotlin/asciiValue.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+
+    val char = 'a'
+    val ascii = char.code
+
+    println("The ASCII value of $char is: $ascii")
+}
---
+++ src/main/kotlin/floatMultiplication.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var firstNumber = 23.5
+    var secondNumber = 0.5
+
+   var multiply = firstNumber * secondNumber;
+    print("multiplication = $multiply")
+
+}
