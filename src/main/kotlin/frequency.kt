659041007feb0201bcdf5fb4575bd441cf62cb33
Yetife <tifetreats21@gmail.com>
2022-02-07 11:29:19 +0100

    Added new kotlin exercise

--- src/main/kotlin/frequency.kt
+++ src/main/kotlin/frequency.kt
@@ -1,9 +1,13 @@
+import java.util.Scanner
 fun main(args: Array<String>) {
-    val str = "Kotlin is a great language";
-    val char = 'a';
+    val sentence = "Kotlin is a great language";
     var frequency = 0;
 
-    for (element in str) {
+    val input = Scanner(System.`in`)
+    print("Enter a character - ")
+    val char = input.next().single()
+
+    for (element in sentence) {
         if (char == element) {
             ++frequency
         }
