da9bb3831904d7e897ddd6fd649d8ac80a89d875
Yetife <tifetreats21@gmail.com>
2022-02-13 16:34:49 +0100

    Added new kotlin exercise

---
+++ src/main/kotlin/exercises/pangram.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main() {
+    var words = ""
+
+    val input = Scanner(System.`in`);
+    println("Enter words: ")
+    words = input.next()
+
+    println("${pangram(words)} $words")
+}
+fun pangram(words: String): Boolean {
+    val lowerString = words.map { it.lowercaseChar() }
+    return ('a'..'z').all {lowerString.contains(it)}
+}
