b0aaafbbcf7baaa421fe3a063d411c9a2c42650d
Yetife <tifetreats21@gmail.com>
2022-02-21 15:18:00 +0100

    Added new kotlin exercise

--- src/main/kotlin/leapYear.kt
+++ src/main/kotlin/leapYear.kt
@@ -1,14 +1,9 @@
 fun main(args: Array<String>) {
-    val year = 1900
-    var leap = false
+    val year = 2020
 
-    leap = if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            year % 400 == 0
-        } else
-            true
-    } else
-        false
+    when(year % 4 == 0 || year % 400 == 0) {
+        true -> println("$year is a leap year")
+        else -> println("$year is not a leap year")
+    }
 
-    println(if (leap) "$year is a leap year." else "$year is not a leap year.")
 }
